bdb0f8a7ac3f8de202e4fc742eb0e2797298d011
Gerard E. Paul <gerardpaul@outlook.com>
2022-01-08 11:38:41 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let letter = ('Hello World');
+let country; 
+let continent;
+let population;
+
+let isIsland = true; 
+continent = isIsland;
+if(continent){isIsland = true; console.log('This is an island.')}
+else console.log('This is not an island.')
+
+isIsland = country; 
+
+
