9cd22112cea9d5441a07df4584a66bbce8fdf02a
Gerard E. Paul <gerardpaul@outlook.com>
2022-01-12 18:10:54 -0600

    Functions: Coding Excersises

--- Functions/script.js
+++ Functions/script.js
@@ -1,20 +1,37 @@
 "use strict";
 /* This sections discusses fucntions implementation in JavaScript.
  */
-
-function CalcuteAge(birthyear) {
-  const currentyear = 2022;
-  let age = currentyear - birthyear;
-
-  return age;
+function GetIntert() {
+  const Interest = prompt(
+    `What would you like to know about Gerard?',
+  'job,`,
+    "age",
+    "firstname",
+    "lastname",
+    "friends"
+  );
 }
 
-let NewAge = CalcuteAge(1976);
-console.log(NewAge);
+// Coding Challenge:
+//Tip Calculator:
+function CalcutlateTip(amount) {
+  let tip;
+  let newvalue = amount;
+
+  if (amount > 0 && amount >= 50 && amount <= 300) {
+    tip = 0.15;
+    newvalue *= tip;
+  } else {
+    tip = 0.2;
+    newvalue *= tip;
+  }
+  return newvalue;
+}
+
+// Coding Challenge:
 
 //Arrow Functions: Used for simple functions
 const CalcutaleAge = (birthyear) => 2022 - birthyear;
-console.log(CalcuteAge(1976));
 
 // Arrow Funstions with more parameters: remember to incase values in paremthasis.
 const YearsUntilRetirement = (birthyear) => {
@@ -22,8 +39,6 @@ const YearsUntilRetirement = (birthyear) => {
   const retirment = 65 - age;
   return retirment;
 };
-
-console.log(YearsUntilRetirement(1964));
 
 // More Functions
 function CutFruitPieces(fruit) {
@@ -36,5 +51,117 @@ function FruitProccessor(apples, oranges) {
 
   return juice;
 }
+let AverageScore = [Number("")];
+// Coding Challenge
+function CalculateAverage(amount) {
+  let counter = 0;
+  const scores = [];
+  let index = 0;
+  while (counter <= 2) {
+    amount += prompt("What are doing?");
+    scores.push(`${amount}`);
 
-// Coding Challenge:
+    counter++;
+  }
+  amount + 1;
+
+  for (let i = 0; i < scores.length; ++i) {
+    let newscore = scores.push(i);
+    console.log(`The total average is: ${newscore[i]}`);
+  }
+  return scores;
+}
+// Coding Challenge
+//------------// Arrays //--------------------------------------------Arrays------------//
+
+// the two most important data strctures are arrays and onjects
+
+// two ways of declaring an array.
+const friends = ["Tony: ", "andre: ", "Pooch: "];
+const years = new Array(1991, 1984, 2008);
+
+//print the element
+console.log(friends[0]);
+//get the length of the array
+console.log(friends.length); // to get length: -1
+
+// insert an element
+friends[1] = "Andre: ";
+console.log(friends);
+
+//Array Operations
+
+friends.push("Mad Dog: ");
+
+const newLength = friends.push("Don Won: "); // this captures the length in a veriable.
+
+friends.unshift("Marry: "); // adds an element to the beginning of the array.
+// friends.pop(); take out the last element.
+// friends.IndexOf('Marry'); returns the element at this index.
+// friends.includes('element') returns a boolean value.
+
+// Coding Challenge
+
+//Coding Challenge
+
+//------------//Arrays
+
+//-------------------//Objects
+
+//Data structures:
+const GerardArray = [];
+
+//object construction
+const Gerard = {
+  // key value pairs are called properties.
+  // fucntions are also properties along with all other properties.
+  firstname: "Gerard",
+  lastname: "Paul",
+  birthdate: 1976,
+  job: "Freelancer",
+  friends: friends,
+  hasFriends: true,
+
+  //adding functions to objects:
+  calculateAge: function (birthyear) {
+    return 2020 - this.age;
+  },
+};
+
+// retrieve and change  Data:
+console.log(Gerard.lastname);
+// or bracket notation. Allows you to use expressions.
+console.log(Gerard["lastname"]);
+
+// expression example:
+const nameKey = "name";
+console.log(Gerard["first" + nameKey]);
+console.log(Gerard["last" + nameKey]);
+
+// to get information
+
+//console.log(Gerard[Interest]);
+
+// -||Challenge: write a sentence in a dynamic way.
+
+// -||Challeng write a string that contains all of the data for this object.
+
+// -||Start Coding: Challenge.
+
+// -||End Coding: Challenge.
+
+const types = [];
+// for loops
+
+for (let i = 0; i <= friends.length; ++i) {
+  console.log(`${friends[i - 1]}` + `${i}`);
+
+  //fill an array.
+  types.push(typeof friends[i]);
+}
+
+// for loops
+console.log(CalcutlateTip(70));
+
+//-------------------//Objects
+CalculateAverage(AverageScore);
